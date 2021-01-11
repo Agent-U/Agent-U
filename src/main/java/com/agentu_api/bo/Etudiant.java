fb6f87d87322c324fedb5dce318f3d8c6d0a73a6
arab <nassimarab@gmail.com>
2021-01-11 10:09:32 +0100

    feat: add Entity Etudiant with Test

---
+++ src/main/java/com/agentu_api/bo/Etudiant.java
@@ -0,0 +1,113 @@
+package com.agentu_api.bo;
+
+import org.springframework.lang.NonNull;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Etudiant {
+
+    @Id
+    @NonNull
+    private String ine;
+    private String nom;
+    private String prenom;
+    private String mail;
+    private String motDePasse;
+    private int petiteEnveloppe;
+    private int grandeEnveloppe;
+    private int avisPassage ;
+    private int colis;
+    private String chambre;
+
+
+
+    public Etudiant() {
+
+    }
+
+    public String getIne() {
+        return ine;
+    }
+
+    public void setIne(String ine) {
+        this.ine = ine;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getMotDePasse() {
+        return motDePasse;
+    }
+
+    public void setMotDePasse(String motDePasse) {
+        this.motDePasse = motDePasse;
+    }
+
+    public int getPetiteEnveloppe() {
+        return petiteEnveloppe;
+    }
+
+    public void setPetiteEnveloppe(int petiteEnveloppe) {
+        this.petiteEnveloppe = petiteEnveloppe;
+    }
+
+    public int getGrandeEnveloppe() {
+        return grandeEnveloppe;
+    }
+
+    public void setGrandeEnveloppe(int grandeEnveloppe) {
+        this.grandeEnveloppe = grandeEnveloppe;
+    }
+
+    public int getAvisPassage() {
+        return avisPassage;
+    }
+
+    public void setAvisPassage(int avisPassage) {
+        this.avisPassage = avisPassage;
+    }
+
+    public int getColis() {
+        return colis;
+    }
+
+    public void setColis(int colis) {
+        this.colis = colis;
+    }
+
+    public String getChambre() {
+        return chambre;
+    }
+
+    public void setChambre(String chambre) {
+        this.chambre = chambre;
+    }
+
+
+
+
+}
